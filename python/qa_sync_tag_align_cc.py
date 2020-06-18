6b89761db96e2cccb1fd4ef5909286d0bd169d76
Johannes Demel <demel@ant.uni-bremen.de>
2020-06-18 15:14:05 +0200

    qa: Add QA code for sync_tag_align

--- python/qa_sync_tag_align_cc.py
+++ python/qa_sync_tag_align_cc.py
@@ -9,9 +9,11 @@
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 import xfdm_sync_swig as xfdm_sync
+import pmt
+import numpy as np
+
 
 class qa_sync_tag_align_cc(gr_unittest.TestCase):
-
     def setUp(self):
         self.tb = gr.top_block()
 
@@ -19,10 +21,42 @@ class qa_sync_tag_align_cc(gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t(self):
-        # set up fg
+        sync_key = "foo"
+        n_streams = 4
+
+        tagger = xfdm_sync.sync_tag_align_cc(n_streams, sync_key)
+
+        tag = gr.python_to_tag(
+            (3000, pmt.intern(sync_key), pmt.PMT_T, pmt.intern("qa"))
+        )
+
+        srcs = []
+        snks = []
+        testdata = []
+        for i in range(n_streams):
+            d = (np.arange(12000) + i) * (i + 1)
+            d = d.astype(np.complex64) * (1.0 + 1.0j)
+            testdata.append(d)
+            src = blocks.vector_source_c(d.tolist())
+            snk = blocks.vector_sink_c()
+            self.tb.connect(src, (tagger, i), snk)
+            srcs.append(src)
+            snks.append(snk)
+        srcs[0].set_data(testdata[0].tolist(), (tag,))
+
         self.tb.run()
+
         # check data
+        for d, s in zip(testdata, snks):
+            r = np.array(s.data())
+            self.assertComplexTuplesAlmostEqual(r, d)
+
+        for s in snks:
+            for t in s.tags():
+                self.assertEqual(t.offset, tag.offset)
+                self.assertTrue(pmt.eq(t.key, tag.key))
+                self.assertTrue(pmt.eq(t.value, tag.value))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gr_unittest.run(qa_sync_tag_align_cc)
